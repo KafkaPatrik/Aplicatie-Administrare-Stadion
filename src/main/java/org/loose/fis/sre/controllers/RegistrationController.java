1d12c4f028c56e45780ccadccd418f0ea2275cfc
Patrik Kafka <patrikafka@gmail.com>
2022-04-10 00:40:41 +0300

    AAS-5 Inregistrare

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -21,14 +21,14 @@ public class RegistrationController {
 
     @FXML
     public void initialize() {
-        role.getItems().addAll("Client", "Admin");
+        role.getItems().addAll("Client", "Administrator");
     }
 
     @FXML
     public void handleRegisterAction() {
         try {
             UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            registrationMessage.setText("Account created successfully!");
+            registrationMessage.setText("Contul a fost creat cu succes!");
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
         }
