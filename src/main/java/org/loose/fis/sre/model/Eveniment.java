38f6f17530c5ca5c39f1b27345bd47838f2311ab
Patrik Kafka <patrikafka@gmail.com>
2022-05-15 15:34:22 +0300

    AAS-24 Adaugat Model Eveniment

---
+++ src/main/java/org/loose/fis/sre/model/Eveniment.java
@@ -0,0 +1,78 @@
+package org.loose.fis.sre.model;
+
+import org.dizitart.no2.objects.Id;
+
+public class Eveniment{
+    @Id
+    private int event_Id;
+    private int event_max_participants;
+    private String event_Title;
+    private String event_Location;
+    private String event_Date;
+    private String event_Description;
+
+    public void set_event_Id(int event_Id) {
+        this.event_Id =event_Id;
+    }
+    public void set_event_max_participants(int event_max_participants) {
+        this.event_max_participants =event_max_participants;
+    }
+    public void set_event_Title(String event_Title) {
+        this.event_Title =event_Title;
+    }
+    public void set_event_Location(String event_Location) {
+        this.event_Location =event_Location;
+    }
+    public void set_event_Date(String event_Date) {
+        this.event_Date =event_Date;
+    }
+    public void set_event_Description(String event_Description) {
+        this.event_Description =event_Description;
+    }
+
+    public Eveniment(int event_Id,int event_max_participants,String event_Title,String event_Location,String event_Date,String event_Description) {
+        this.event_max_participants =event_max_participants;
+        this.event_Title =event_Title;
+        this.event_Location =event_Location;
+        this.event_Date =event_Date;
+        this.event_Description =event_Description;
+    }
+
+    public Eveniment() {
+    }
+
+    public int get_event_Id() {return event_Id;}
+
+    public int get_event_max_participants() {
+        return event_max_participants;
+    }
+
+    public String get_event_Title(String username) {return event_Title;}
+
+    public String get_event_Location() {
+        return event_Date;
+    }
+
+    public String get_event_Date(String password) {return event_Date;}
+
+    public String get_event_Description() {return event_Description;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Eveniment eveniment = (Eveniment) o;
+        if(!(event_Id==eveniment.event_Id))  return false;
+        if (event_Title != null ? !event_Title.equals(eveniment.event_Title) : eveniment.event_Title != null) return false;
+        return true;
+    }
+    @Override
+    public int hashCode() {
+        int result = event_Title != null ? event_Title.hashCode() : 0;
+        result = 31 * result + (event_Date != null ? event_Date.hashCode() : 0);
+        result = 31 * result + (event_Location != null ? event_Location.hashCode() : 0);
+        return result;
+    }
+
+}
---
+++ src/main/java/org/loose/fis/sre/services/EventService.java
@@ -0,0 +1,100 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.model.Eveniment;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;/*
+public class EventService {
+
+    private static ObjectRepository<Eveniment> userRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("users-database.db").toFile())
+                .openOrCreate("test", "test");
+
+        userRepository = database.getRepository(User.class);
+    }
+
+    public static void addUser(String username, String password, String role) throws UsernameAlreadyExistsException {
+        checkUserDoesNotAlreadyExist(username);
+        userRepository.insert(new User(username, encodePassword(username, password), role));
+    }
+
+    private static void checkUserDoesNotAlreadyExist(String username) throws UsernameAlreadyExistsException {
+        for (User user : userRepository.find()) {
+            if (Objects.equals(username, user.getUsername()))
+                throw new UsernameAlreadyExistsException(username);
+        }
+    }
+
+    private static String encodePassword(String salt, String password) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        // This is the way a password should be encoded when checking the credentials
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", ""); //to be able to save in JSON format
+    }
+
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-512 does not exist!");
+        }
+        return md;
+    }
+
+
+    public static String validateLogin(String username, String password) {
+        for (User user : userRepository.find()) {
+            if(username.equals(user.getUsername()))
+            {   String encodedPassword=encodePassword(username,password);
+                if (encodedPassword.equals(user.getPassword())) {
+                    return "Valid";
+                }
+            }
+        }
+        return "Invalid";
+    }
+
+
+    public static boolean modifyClientAccountInfo (String username, String password, String email, String firstName, String lastName, String phoneNumber) {
+        for (User user : userRepository.find()) {
+            if(username.equals(user.getUsername()))
+            {   String encodedPassword=encodePassword(username,password);
+                if (encodedPassword.equals(user.getPassword())) {
+                    user.setEmail(email);
+                    user.setFirstName(firstName);
+                    user.setLastName(lastName);
+                    user.setPhoneNumber(phoneNumber);
+                    userRepository.update(user);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static User returnCurrentUser (String username, String password){
+        for (User user : userRepository.find()) {
+            if(username.equals(user.getUsername()))
+            {   String encodedPassword=encodePassword(username,password);
+                if (encodedPassword.equals(user.getPassword())) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+}*/
