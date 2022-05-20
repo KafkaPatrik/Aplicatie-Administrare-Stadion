63aa5acefd8b999249ebad23c5f627808c17ea4b
Iulia K <iulia.kovacs@student.upt.ro>
2022-05-20 16:04:38 +0300

    AAS-23 Created tickets database

---
+++ src/main/java/org/loose/fis/sre/model/Ticket.java
@@ -0,0 +1,78 @@
+package org.loose.fis.sre.model;
+
+import org.dizitart.no2.objects.Id;
+import org.loose.fis.sre.controllers.LoginController;
+
+public class Ticket {
+    @Id
+    private String idCode;
+    private String buyerUsername;
+    private String ticketOwnerName;
+    private String phoneNumber;
+    private String purchaseDateTimeStamp;
+    private String category;
+    private double price;
+    private boolean hasParkingSpot;
+    private String parkingSpot;
+    private int id_event;
+
+    public Ticket(String idCode, String ticketOwnerName, String phoneNumber, String purchaseDate, String category, double price, boolean hasParkingSpot, String parkingSpot, int id_event) {
+        this.idCode = idCode;
+        this.ticketOwnerName = ticketOwnerName;
+        this.phoneNumber = phoneNumber;
+        this.purchaseDateTimeStamp = purchaseDate;
+        this.category = category;
+        this.price = price;
+        this.hasParkingSpot = hasParkingSpot;
+        this.parkingSpot = parkingSpot;
+        this.id_event=id_event;
+        this.buyerUsername=LoginController.current_user.getUsername();
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public String getTicketOwnerName() {
+        return ticketOwnerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDateTimeStamp;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isHasParkingSpot() {
+        return hasParkingSpot;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public String getBuyerUsername() { return buyerUsername; }
+
+    public void setBuyerUsername(String buyerUsername) { this.buyerUsername = buyerUsername; }
+
+    public  Ticket(){
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Ticket)
+            return (((Ticket) o).getIdCode().equals(idCode));
+        else return false;
+    }
+
+}
---
+++ src/main/java/org/loose/fis/sre/services/TicketService.java
@@ -0,0 +1,48 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.exceptions.TicketAlreadyExistsException;
+import org.loose.fis.sre.model.Ticket;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
+
+public class TicketService {
+
+    private static ObjectRepository<Ticket> ticketRepository;
+
+    public static void initTicketsDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("tickets-database.db").toFile())
+                .openOrCreate("test", "test");
+
+        ticketRepository = database.getRepository(Ticket.class);
+    }
+
+    public static void addTicket(String id_code, String ticketOwnerName, String phoneNumber, String category, double price, boolean hasParkingSpot, String parkingSpot, int id_event) throws TicketAlreadyExistsException {
+        checkTicketDoesNotAlreadyExist(id_code);
+        String purchaseDateTimeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+        System.out.println(purchaseDateTimeStamp);
+        ticketRepository.insert(new Ticket(id_code, ticketOwnerName, phoneNumber, purchaseDateTimeStamp, category, price, hasParkingSpot, parkingSpot, id_event));
+    }
+
+    private static void checkTicketDoesNotAlreadyExist(String id_code) throws TicketAlreadyExistsException {
+        for (Ticket ticket : ticketRepository.find()) {
+            if (id_code.equals(ticket.getIdCode()))
+                throw new TicketAlreadyExistsException(id_code);
+        }
+    }
+
+    public static Ticket returnTicket (String ticketID){
+        for (Ticket ticket : ticketRepository.find()) {
+            if (ticketID.equals(ticket.getIdCode()))
+               return ticket;
+        }
+        return null;
+    }
+
+}
