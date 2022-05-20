19fddf0fa114471b3e4450a1db62a14ddb9f120f
Iulia K <iulia.kovacs@student.upt.ro>
2022-05-20 11:04:43 +0300

    New changes in interface

--- src/main/java/org/loose/fis/sre/controllers/ClientHomePageController.java
+++ src/main/java/org/loose/fis/sre/controllers/ClientHomePageController.java
@@ -7,18 +7,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import org.loose.fis.sre.model.User;
 import org.loose.fis.sre.services.EventService;
 import org.loose.fis.sre.model.Eveniment;
 
+import java.util.Locale;
+import java.util.Objects;
 import java.io.IOException;
 import javafx.scene.text.Text;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 
-public class AdminHomePageController {
+public class ClientHomePageController {
 
     @FXML
     private Button btnLogOut;
@@ -27,15 +33,11 @@ public class AdminHomePageController {
     @FXML
     private Button btnAccountInfo;
     @FXML
-    private Button btnShowSelection;
-    @FXML
-    private Button btnShowList;
-    @FXML
     private Text selectionMessage;
     @FXML
     private ListView<String> list;
+    @FXML
     private ObservableList<String> items=FXCollections.observableArrayList();
-    public static Eveniment current_selected_event;
 
     public void initialize(){
         items.removeAll();
@@ -43,25 +45,23 @@ public class AdminHomePageController {
         list.setItems(items);
     }
 
+
     public void handleLoggingOut(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         Stage window = (Stage) btnLogOut.getScene().getWindow();
         window.setScene(new Scene(root, 600, 450));
     }
 
+
     public void handleHomePageAction (ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("homePageAdmin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("homePageClient.fxml"));
         Stage window = (Stage) btnHomePage.getScene().getWindow();
         window.setScene(new Scene(root, 600, 450));
     }
 
+
     public void handleAccountInfoAction(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminAccountInfoAccess.fxml"));
-        Stage window = (Stage) btnAccountInfo.getScene().getWindow();
-        window.setScene(new Scene(root, 600, 450));
-    }
-    public void handleAddEventAction(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("adminAddEvent.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("clientAccountInfoAccess.fxml"));
         Stage window = (Stage) btnAccountInfo.getScene().getWindow();
         window.setScene(new Scene(root, 600, 450));
     }
@@ -73,7 +73,6 @@ public class AdminHomePageController {
         selection="Ați selectat:  " + selection;
         selectionMessage.setText(selection);
     }
-
 
 }
 
