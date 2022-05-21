ef9a755a85d926612fdb88afad7d305e01bf4d8a
Iulia K <iulia.kovacs@student.upt.ro>
2022-05-21 14:50:58 +0300

    AAS-55 Added validate login function test

--- src/main/java/tests/services/UserServiceTest.java
+++ src/main/java/tests/services/UserServiceTest.java
@@ -1,4 +1,30 @@
 package tests.services;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.loose.fis.sre.services.FileSystemService;
+import org.loose.fis.sre.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserServiceTest {
+
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+        UserService.initDatabase();
+    }
+
+    @Test
+    void testLogIn() {
+        assertThat(UserService.validateLogin(USERNAME,PASSWORD).equals("Valid"));
+    }
 }
