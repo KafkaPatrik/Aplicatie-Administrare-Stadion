4fb3df5111eff62c980ca4d190ac2119225b75e3
Patrik Kafka <patrikafka@gmail.com>
2022-05-22 21:25:57 +0300

    AAS-25 Edited interface.

--- src/main/java/org/loose/fis/sre/controllers/Admin/AdminAccountAccessController.java
+++ src/main/java/org/loose/fis/sre/controllers/Admin/AdminAccountAccessController.java
@@ -39,6 +39,14 @@ public class AdminAccountAccessController {
     @FXML
     private Label roleText;
     @FXML
+    private Label sadrText;
+    @FXML
+    private Label capText;
+    @FXML
+    private Label snameText;
+    @FXML
+    private Label ownnText;
+    @FXML
     private Text warningNewAccountMessage;
 
 
@@ -67,6 +75,10 @@ public class AdminAccountAccessController {
                 firstNameText.setText(current_user.getFirstName());
                 lastNameText.setText(current_user.getLastName());
                 phoneNumberText.setText(current_user.getPhoneNumber());
+                sadrText.setText(current_user.getStadiumAdress());
+                capText.setText(current_user.getCapacity());
+                snameText.setText(current_user.getStadiumName());
+                ownnText.setText(current_user.getOwnerName());
                 roleText.setText("Rol: " + current_user.getRole());
                 if (current_user.getEmail().length()==0)
                     warningNewAccountMessage.setText("Trebuie completate datele contului\ndeoarece dețineți un cont nou creat!");
